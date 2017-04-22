49a5d6c1d75c1fef2d18f8c289dd4310d4dc808c
J-C <jcjoneskern@gmail.com>
2017-04-21 21:33:16 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var circle = document.getElementById('circle');
+var text = document.getElementById('text');
+
+function getRad() {
+  var value = parseInt(document.getElementById('slider').value);
+  var radius = (75*value)/16777215;
+  var hex = value.toString(16);
+
+  function areaCalc() {
+    var area = Math.PI * radius * radius;
+
+    if(value === 0) {
+      text.innerText = "The area of the circle is " + area.toFixed() + " units =(";
+      circle.style.border = "none";
+    } else {
+      text.innerText = "The area of the circle is " + area.toFixed() + " units";
+      circle.style.border = "1px solid #000";
+    }
+  }
+
+  circle.style.height = radius + "vh";
+  circle.style.width = circle.style.height;
+  circle.style.backgroundColor = "#" + hex;
+  areaCalc();
+}
+
+
+document.onload = getRad();
